1e0c7983a4e014bdd44fc26b67c547d5806553e5
Tyler Davis <tyebud@gmail.com>
2013-02-14 20:38:57 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+#TylerScript
+##Exceptions that make sense
+
+Feel free to fork this amazing resource to further the cause
+
+###Instructions
+Each exception's to_s method is overwritten.  The responses array is then queried, which returns a random, yet informative exception, that will definitely expedite your debugging.
---
+++ tyler.rb
@@ -0,0 +1,195 @@
+class Exception
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class NoMemoryError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class ScriptError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class LoadError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class NotImplementedError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SyntaxError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SignalException
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class Interrupt
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class StandardError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class ArgumentError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class IOError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class EOFError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class IndexError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class LocalJumpError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class NameError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class NoMethodError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class RangeError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class FloatDomainError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class RegexpError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class RuntimeError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SecurityError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SystemCallError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SystemStackError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class ThreadError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class TypeError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class ZeroDivisionError
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class SystemExit
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
+
+class fatal
+  def to_s
+    responses = ["I've got nothing."]
+    return responses[rand(responses.length-1)]
+  end
+end
