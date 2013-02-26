4152c7543d9dd27edebca849a0e8371e5149912e
Steve Klabnik <steve@steveklabnik.com>
2013-02-26 12:23:26 -0800

    Metaprogramming.
    I'm not sure if you learned anything about this yet, but if not... ;)

--- lib/tyler.rb
+++ lib/tyler.rb
@@ -1,188 +1,34 @@
-class Exception
-  def to_s
-    responses = ["I've got nothing.", "Good job, genius."]
-    return responses.sample
-  end
-end
-
-class NoMemoryError
-  def to_s
-    responses = ["I've got nothing.", "Fuggedaboudit.", "Amnesia, or are you just an idiot?"]
-    return responses.sample
-  end
-end
-
-class ScriptError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class LoadError
-  def to_s
-    responses = ["What did the module say to the class?  We'll never know because you can't type.", "Barking up the wrong tree, dumbass."]
-    return responses.sample
-  end
-end
-
-class NotImplementedError
-  def to_s
-    responses = ["Are you using Windows?  You're using Windows, aren't you?"]
-    return responses.sample
-  end
-end
-
-class SyntaxError
-  def to_s
-    responses = ["I've got nothing.", "Lololololololwut", "Aw, how cute, you broke Ruby.", "Wow. I knew you were special, but wow."]
-    return responses.sample
-  end
-end
-
-class SignalException
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class Interrupt
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class StandardError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class ArgumentError
-  def to_s
-    responses = ["I've got nothing.", "Wrong number of arguments. Can you even count?"]
-    return responses.sample
-  end
-end
-
-class IOError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class EOFError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class IndexError
-  def to_s
-    responses = ["Index error. Talk to me again when you've graduated kindergarten."]
-    return responses.sample
-  end
-end
-
-class LocalJumpError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class NameError
-  def to_s
-    responses = ["Wrong! Ha. Ha ha. I'm not laughing at you, I mean, well, yes I am."]
-    return responses.sample
-  end
-end
-
-class NoMethodError
-  def to_s
-    responses = ["Undefined method. That thing you're trying to do... No."]
-    return responses.sample
-  end
-end
-
-class RangeError
-  def to_s
-    responses = ["I've got nothing.", "Counting. You're doing it wrong."]
-    return responses.sample
-  end
-end
-
-class FloatDomainError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class RegexpError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class RuntimeError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class SecurityError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class SystemCallError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class SystemStackError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class ThreadError
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
-  end
-end
-
-class TypeError
-  def to_s
-    responses = ["Wrong. Just wrong.", "Are you touched in the head or what?"]
-    return responses.sample
-  end
-end
-
-class ZeroDivisionError
-  def to_s
-    responses = ["Dividing by zero. Who the fuck do you think you are?"]
-    return responses.sample
-  end
-end
-
-class SystemExit
-  def to_s
-    responses = ["I've got nothing."]
-    return responses.sample
+{
+  Exception => ["I've got nothing.", "Good job, genius."],
+  NoMemoryError => ["I've got nothing.", "Fuggedaboudit.", "Amnesia, or are you just an idiot?"],
+  ScriptError => ["I've got nothing."],
+  LoadError => ["What did the module say to the class?  We'll never know because you can't type.", "Barking up the wrong tree, dumbass."],
+  NotImplementedError => ["Are you using Windows?  You're using Windows, aren't you?"],
+  SyntaxError => ["I've got nothing.", "Lololololololwut", "Aw, how cute, you broke Ruby.", "Wow. I knew you were special, but wow."],
+  SignalException => ["I've got nothing."],
+  Interrupt => ["I've got nothing."],
+  StandardError => ["I've got nothing."],
+  ArgumentError => ["I've got nothing.", "Wrong number of arguments. Can you even count?"],
+  IOError => ["I've got nothing."],
+  EOFError => ["I've got nothing."],
+  IndexError => ["Index error. Talk to me again when you've graduated kindergarten."],
+  LocalJumpError => ["I've got nothing."],
+  NameError => ["Wrong! Ha. Ha ha. I'm not laughing at you, I mean, well, yes I am."],
+  NoMethodError => ["Undefined method. That thing you're trying to do... No."],
+  RangeError => ["I've got nothing.", "Counting. You're doing it wrong."],
+  FloatDomainError => ["I've got nothing."],
+  RegexpError => ["I've got nothing."],
+  RuntimeError => ["I've got nothing."],
+  SecurityError => ["I've got nothing."],
+  SystemCallError => ["I've got nothing."],
+  SystemStackError => ["I've got nothing."],
+  ThreadError => ["I've got nothing."],
+  TypeError => ["Wrong. Just wrong.", "Are you touched in the head or what?"],
+  ZeroDivisionError => ["Dividing by zero. Who the fuck do you think you are?"]
+}.each do |klass, responses|
+  klass.class_eval do
+    define_method :to_s do
+      responses.sample
+    end
   end
 end
